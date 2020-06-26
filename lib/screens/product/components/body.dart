d3ec20a76ce2be0b9fd1216a6ce1369e82847a6f
Pedrowesley <raquel97378117@gmail.com>
2020-06-25 21:38:32 -0300

    Colocando imagem com texto em card de produto

--- lib/screens/product/components/body.dart
+++ lib/screens/product/components/body.dart
@@ -24,36 +24,81 @@ class Body extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: kDefaultPadding,
-                  vertical: kDefaultPadding / 2,
-                ),
-                height: 160,
-                // color: Colors.blueAccent,
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
+              ProductCard(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 2,
+      ),
+      height: 160,
+      // color: Colors.blueAccent,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Container(
+            height: 136,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(22),
+              color: kBlueColor,
+              boxShadow: [kDefaultShadow],
+            ),
+            child: Container(
+              margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(22)),
+            ),
+          ),
+          // Product image
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 160,
+              width: 200,
+              child: Image.asset(
+                "assets/images/Item_1.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              child: SizedBox(
+                height: 136,
+                width: size.width - 200,
+                child: Column(
                   children: <Widget>[
-                    Container(
-                      height: 136,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(22),
-                        color: kBlueColor,
-                      ),
-                      child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(22)),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding),
+                      child: Text(
+                        "Classic Leather Arm Chair",
+                        style: Theme.of(context).textTheme.button,
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
-          ),
-        )
-      ],
+              ))
+        ],
+      ),
     );
   }
 }
